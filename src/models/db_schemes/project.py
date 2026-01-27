a9ae268a614a2d9ae047cff165eeffd5431a57ab
Mohamed Mosad <mohamedmosad1223@gmail.com>
2026-01-27 20:15:27 +0200

    create docker compose for mongodb and prepare database

---
+++ src/models/db_schemes/project.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel,Field,validator
+from typing import Optional
+from bson.objectid import ObjectId
+class Project(BaseModel):
+    _id: Optional[ObjectId] = Field(None, alias="_id")
+    project_id: str = Field(..., min_length=1)
+
+
+    @validator("project_id")
+    def validate_project_id(cls, v):
+        if not v.isalnum():
+            raise ValueError("project_id must be alphanumeric")
+        return v
+
+    class Config:
+        arbitrary_types_allowed = True
+
