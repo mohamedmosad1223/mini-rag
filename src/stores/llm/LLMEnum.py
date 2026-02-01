8ce431f6d0baf36aa48f3fd80d716106331e0062
Mohamed Mosad <mohamedmosad1223@gmail.com>
2026-02-01 20:53:44 +0200

    create a factory

--- src/stores/llm/LLMEnum.py
+++ src/stores/llm/LLMEnum.py
@@ -8,3 +8,15 @@ class OpenAiModelsEnum(str, Enum):
     SYSTEM="system"
     USER="user"
     ASSISTANT="assistant"
+
+class CohereModelsEnum(str, Enum):
+    SYSTEM="SYSTEM"
+    USER="USER"
+    ASSISTANT="CHATBOT"
+    DOCUMENT= "search_document"
+    QUERY="search_query"
+
+class DocumentTypeEnum(str, Enum):
+    DOCUMENT="document"
+    QUERY="query"
+    
---
+++ src/stores/llm/providers/CoHereProvider.py
@@ -0,0 +1,93 @@
+from ..LLMInterface import LLMInterface
+from openai import OpenAI
+import logging
+from ..LLMEnum import CohereModelsEnum, DocumentTypeEnum
+import cohere
+
+class CoHereProvider(LLMInterface):
+    def __init__(self, api_key: str,api_url:str=None,
+                 default_input_max_characters :int =1000
+                 ,default_generation_max_output_tokens :int =1000,
+                 default_generation_temperature: float =0.1,):
+        
+        self.api_key = api_key
+
+        self.default_input_max_characters = default_input_max_characters
+        self.default_generation_max_output_tokens = default_generation_max_output_tokens
+        self.default_generation_temperature = default_generation_temperature
+
+        self.generation_model_id=None
+
+        self.embedding_model_id=None
+        self.embedding_size=None
+
+        self.client = cohere.Client(
+            api_key=self.api_key,             
+            )
+        self.logger = logging.getLogger(__name__)
+
+    def set_generation_model(self, model_id: str):
+        self.generation_model_id = model_id
+
+
+    def set_embedding_model(self, model_id: str, embedding_size: int):
+        self.embedding_model_id = model_id
+        self.embedding_size = embedding_size
+
+    def process_text(self,text:str):
+        return text[:self.default_input_max_characters].strip()
+    def generate_text(self, prompt: str,chat_history: list=None, max_output_tokens: int=None, temperature: float =None):
+        if not self.client:
+            # raise ValueError("OpenAI client is not initialized.") but it stop the program
+            self.logger.error("Cohere client is not initialized.") # error log but continue the program
+            return None
+        if not self.generation_model_id:
+            self.logger.error("Generation model ID is not set.")
+            return None
+        response= self.client.chat(
+            model=self.generation_model_id,
+            chat_history=chat_history,
+            message=self.process_text(prompt),
+            max_output_tokens = max_output_tokens if max_output_tokens is not None else self.default_generation_max_output_tokens,
+            temperature = temperature if temperature is not None else self.default_generation_temperature
+
+
+        )
+        if not response or not response.text:
+            self.logger.error("No completion data returned from Cohere.")
+            return None
+        return response.text
+
+    
+    def embed_text(self, text: str,document_type: str =None):
+        if not self.client:
+            # raise ValueError("OpenAI client is not initialized.") but it stop the program
+            self.logger.error("OpenAI client is not initialized.") # error log but continue the program
+            return None
+        if not self.embedding_model_id:
+            self.logger.error("Embedding model ID is not set.")
+            return None
+        input_type=CohereModelsEnum.DOCUMENT 
+        if document_type == DocumentTypeEnum.QUERY.value:
+            input_type=CohereModelsEnum.QUERY
+
+        response = self.client.embed(
+            model=self.embedding_model_id,
+            texts=[self.process_text(text)],
+            input_type=input_type,
+            embedding_types=["float"]
+        )
+        if not response or not response.embeddings or response.embeddings.float:
+            self.logger.error("No embedding data returned from Cohere.")
+            return None
+        return response.embeddings[0].float
+
+    def construct_prompt(self, prompt: str, role: str):
+        return [
+            {
+            "role": role,
+            "text": self.process_text(prompt)
+            }
+        ]
+    
+
